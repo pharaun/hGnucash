b4ae9ee59543ced15058c13bebfa4cd404e07400
Anja Berens <pharaun666@gmail.com>
2015-07-21 01:39:26 -0700

    Probably doing this wrong esp re qualified nodes and so on

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,3 @@
--- | A "hello world" example of hexpat that lazily parses a document, printing
--- it to standard out.
-
 import Text.XML.Expat.Tree
 import Text.XML.Expat.Format
 import Text.XML.Expat.Pickle
@@ -13,49 +10,66 @@ import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
 
 main = do
-    let pu = (xpRoot $ xpElemNodes "people" $ xpList xpPerson)
+    args <- getArgs
+    case args of
+        [filename] -> do
+            xml <- process filename
 
-    BL.putStrLn $ pickleXML pu people
-    print $ unpickleXML defaultParseOptions pu $ pickleXML pu people
+            print xml
 
---    args <- getArgs
---    case args of
---        [filename] -> do
---            (xml, mErr) <- process filename
---
---            -- Process document before handling error, so we get lazy processing.
---            BL.hPutStr stdout $ format xml
---            putStrLn ""
---            case mErr of
---                Nothing -> return ()
---                Just err -> do
---                    hPutStrLn stderr $ "XML parse failed: "++show err
---                    exitWith $ ExitFailure 2
---
---        otherwise  -> do
---            hPutStrLn stderr "Usage: helloworld <file.xml>"
---            exitWith $ ExitFailure 1
+        otherwise  -> do
+            hPutStrLn stderr "Usage: helloworld <file.xml>"
+            exitWith $ ExitFailure 1
 
-process :: String -> IO (UNode T.Text, Maybe XMLParseError)
+--process :: String -> IO (UNode T.Text, Maybe XMLParseError)
 process filename = do
     inputText <- BL.readFile filename
-    return $ parse defaultParseOptions inputText
 
--- Pickle
-data Person = Person String Int String deriving Show
+    return $ unpickleXML defaultParseOptions xpGnuCash inputText
 
-people =
-    [ Person "Dave" 27 "A fat thin man with long short hair"
-    , Person "Jane" 21 "Lives in a white house with green windows"
-    ]
 
-xpPerson :: PU [UNode String] Person
-xpPerson =
-    -- How to wrap and unwrap a Person
-    xpWrap (\((name, age), descr) -> Person name age descr,
-        \(Person name age descr) -> ((name, age), descr)) $
-    xpElem "person"
-        (xpPair
-        (xpAttr "name" xpText0)
-        (xpAttr "age" xpickle))
-        (xpContent xpText0)
+data GnuCash = GnuCash
+    { countBook :: Integer
+    , books :: [GnuCashBook]
+    } deriving Show
+
+data GnuCashBook = GnuCashBook
+    { guid :: String
+    , countAccount :: Integer
+    , countTransaction :: Integer
+    }
+    | NoBook
+    deriving Show
+
+
+xpGnuCash :: PU (Node String String) GnuCash
+xpGnuCash =
+    xpWrap
+        ( \(count, books) -> GnuCash count books
+        , \(GnuCash count books) -> (count, books)
+        ) $
+        -- TODO: xpList -> xpListMinLength (of book count)
+        -- TODO: verify type ~= "books"
+        xpRoot (xpElemNodes "gnc-v2" (xpPair (xpElemNodes "gnc:count-data" (xpContent xpPrim)) (xpList xpBooks)))
+
+
+--xpBooks :: PU (Node String String) GnuCashBook
+xpBooks = xpLift NoBook
+
+
+--    xpRoot
+--        (xpElemNodes "gnc-v2"
+--            (xpPair
+--                (xpElem "gnc:count-data" (xpAttr "cd:type" xpText0) (xpContent xpPrim))
+--                (xpListMinLen 1
+--                    (xpElem "gnc:book"
+--                        (xpAttr "version" xpText0)
+--                        (xpTriple
+--                            (xpElem "book:id" (xpAttr "type" xpText0) (xpContent xpPrim))
+--                            (xpElem "gnc:count-data" (xpAttr "type" xpText0) (xpContent xpPrim))
+--                            (xpElem "gnc:count-data" (xpAttr "type" xpText0) (xpContent xpPrim))
+--                        )
+--                    )
+--                )
+--            )
+--        )
