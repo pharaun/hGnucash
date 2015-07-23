4d4830f8313ecb5b0396bfe0a166d72b5045b491
Anja Berens <pharaun666@gmail.com>
2015-07-23 02:39:12 -0700

    Got the unpicling of the data to work but then got stuk on xml parsing, but we have it now, but there's still some namespace mismatch i think, still need to finish implementing the namespace then do the rest of the pickling

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,12 @@
-import Text.XML.Expat.Tree
-import Text.XML.Expat.Format
-import Text.XML.Expat.Pickle
+{-# LANGUAGE OverloadedStrings #-}
+import qualified Text.XML as X
+import qualified Data.XML.Types as XT
+import Data.XML.Pickle
 
 import System.Environment
 import System.Exit
 import System.IO
 
-import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
 
 main = do
@@ -23,53 +23,83 @@ main = do
 
 --process :: String -> IO (UNode T.Text, Maybe XMLParseError)
 process filename = do
-    inputText <- BL.readFile filename
+    inputNode <- X.readFile (X.def {X.psRetainNamespaces = False}) filename
 
-    return $ unpickleXML defaultParseOptions xpGnuCash inputText
+
+    let dat = GnuCash (CountData "book" 1)
+                ( GnuCashBook "2.0.0" (BookId "guid" "3d2281ed92e804792714679c1b0cab5c") (CountData "account" 49) (CountData "transaction" 903)
+                )
+
+    let out = XT.Document (XT.Prologue [] Nothing []) (pickle (xpRoot $ xpUnliftElems xpGnuCash) dat) []
+    let iin = unpickle (xpRoot $ xpUnliftElems xpGnuCash) (XT.documentRoot out)
+
+    print dat
+    putStrLn ""
+    print out
+    putStrLn ""
+    print iin
+    putStrLn ""
+    case iin of
+        Left _  -> print "False"
+        Right x -> print (x == dat)
+    putStrLn ""
+
+    return $ unpickle (xpRoot $ xpUnliftElems xpGnuCash) (X.toXMLElement $ X.documentRoot inputNode)
 
 
 data GnuCash = GnuCash
-    { countBook :: Integer
-    , books :: [GnuCashBook]
-    } deriving Show
+    { countBook :: CountData
+    , books :: GnuCashBook
+    } deriving (Show, Eq)
+
+data CountData = CountData
+    { countType :: T.Text
+    , countAmount :: Integer
+    } deriving (Show, Eq)
 
 data GnuCashBook = GnuCashBook
-    { guid :: String
-    , countAccount :: Integer
-    , countTransaction :: Integer
-    }
-    | NoBook
-    deriving Show
+    { version :: T.Text -- TODO: ADT this
+    , guid :: BookId
+    , countAccount :: CountData
+    , countTransaction :: CountData
+    } deriving (Show, Eq)
+
+data BookId = BookId
+    { idType :: T.Text
+    , idValue :: T.Text -- TODO: ADT the value/type
+    } deriving (Show, Eq)
 
 
-xpGnuCash :: PU (Node String String) GnuCash
+xpGnuCash :: PU [XT.Node] GnuCash
 xpGnuCash =
     xpWrap
-        ( \(count, books) -> GnuCash count books
-        , \(GnuCash count books) -> (count, books)
-        ) $
-        -- TODO: xpList -> xpListMinLength (of book count)
+        (uncurry GnuCash)
+        (\(GnuCash count books) -> (count, books))
         -- TODO: verify type ~= "books"
-        xpRoot (xpElemNodes "gnc-v2" (xpPair (xpElemNodes "gnc:count-data" (xpContent xpPrim)) (xpList xpBooks)))
+        -- TODO: xpBooks -> (xpList0 xpBooks) (for some reason xpList breaks with xpPair)
+        (xpElemNodes "gnc-v2" (xpPair xpCountData xpBooks))
 
 
---xpBooks :: PU (Node String String) GnuCashBook
-xpBooks = xpLift NoBook
+xpCountData :: PU [XT.Node] CountData
+xpCountData =
+    xpWrap
+        (uncurry CountData)
+        (\(CountData cType amount) -> (cType, amount))
+        (xpElem "gnc:count-data" (xpAttr "cd:type" xpText) (xpContent xpPrim))
 
+xpBooks :: PU [XT.Node] GnuCashBook
+xpBooks =
+    xpWrap
+        (\(version, (guid, account, transaction)) -> GnuCashBook version guid account transaction)
+        (\(GnuCashBook version guid account transaction) -> (version, (guid, account, transaction)))
+        (xpElem "gnc:book" (xpAttr "version" xpText)
+            -- TODO: fix up xpCountData (It double reads the 'account' type it should read account then transactions
+            (xpTriple xpBookId xpCountData xpCountData)
+        )
 
---    xpRoot
---        (xpElemNodes "gnc-v2"
---            (xpPair
---                (xpElem "gnc:count-data" (xpAttr "cd:type" xpText0) (xpContent xpPrim))
---                (xpListMinLen 1
---                    (xpElem "gnc:book"
---                        (xpAttr "version" xpText0)
---                        (xpTriple
---                            (xpElem "book:id" (xpAttr "type" xpText0) (xpContent xpPrim))
---                            (xpElem "gnc:count-data" (xpAttr "type" xpText0) (xpContent xpPrim))
---                            (xpElem "gnc:count-data" (xpAttr "type" xpText0) (xpContent xpPrim))
---                        )
---                    )
---                )
---            )
---        )
+xpBookId :: PU [XT.Node] BookId
+xpBookId =
+    xpWrap
+        (uncurry BookId)
+        (\(BookId idType idValue) -> (idType, idValue))
+        (xpElem "book:id" (xpAttr "type" xpText) (xpContent xpText))
